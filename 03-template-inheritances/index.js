e7ba9b52634718159559a5dfd9c62f3a12369d5c
yongsannnn <yong_s@live.com>
2021-02-03 08:13:55 +0000

    added block to three pages

--- 03-template-inheritances/index.js
+++ 03-template-inheritances/index.js
@@ -1,9 +1,13 @@
 const express=require("express") ;
 const hbs = require("hbs");
+const wax = require("wax-on");
 
 let app = express();
-app.set("view engine", "hbs")
 
+app.set("view engine", "hbs");
+
+wax.on(hbs.handlebars);
+wax.setLayoutPath("./views/layouts");
 app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
