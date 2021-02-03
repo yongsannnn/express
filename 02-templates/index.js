9ac4de143077a1b277a43d414ab56ab93b40c577
yongsannnn <yong_s@live.com>
2021-02-03 07:23:39 +0000

    added in 02-template and express.static

--- 02-templates/index.js
+++ 02-templates/index.js
@@ -9,11 +9,31 @@ let app = express();
 
 app.set("view engine", "hbs");
 
+//telling express where to get all the static files name can be any name
+// Add a CSS file and change BG to purple
+// Add a JS file and alert woof woof
+app.use(express.static("public"))
+
 app.get("/", (req,res)=>{
     res.render("index.hbs");
 })
 
 
+app.get("/greeting/:name", (req,res)=>{
+    let fullName = req.params.name;
+    res.render("greeting.hbs",{
+        "fullName" : fullName
+    });
+
+})
+
+// Use Math.random function to generate a number between 1 - 1000 and send it to a view file named lucky.hbs and inform the user that is their lucky number. 
+
+app.get("/luckynum",(req,res)=>{
+    res.render("luckynum.hbs",{
+        "lucky" : (Math.floor(Math.random() * 1000) + 1)
+    })
+})
 
 app.listen(3000, ()=>{
     console.log("Sever Started")
