9c3f9f07d34dd9d3162c972a2a32f76b8e1b659b
yongsannnn <yong_s@live.com>
2021-02-05 03:19:24 +0000

    added comments into the lines for better understanding

--- 04-forms-processing-hands-on-a/index.js
+++ 04-forms-processing-hands-on-a/index.js
@@ -1,21 +1,28 @@
+// Setup Starts
 const express = require('express');
 const hbs = require('hbs');
 const wax = require('wax-on');
 
 let app = express();
-app.set('view engine', 'hbs');
-wax.on(hbs.handlebars);
-wax.setLayoutPath('./views/layouts')
-app.use(express.static('public'));
+app.set('view engine', 'hbs'); // Tell express to use HBS as view engine
 
+wax.on(hbs.handlebars); // For template inheritance
+wax.setLayoutPath('./views/layouts') //Telling wax-on where to find the layout file
+
+app.use(express.static('public')); //Tell Express where to find static files(css,json)
+ 
+//Important line, without this the form will return undefined
 app.use(express.urlencoded({
     extended: false
 }))
 
-app.get('/bmi', (req,res)=>{
+// Setup Ends
+// Route Starts
+// First Route to display the form
+app.get('/bmi', (req,res)=>{   
     res.render('bmi')
 })
-
+// First Route to prcess the form
 app.post("/bmi", (req,res)=>{
     let height = req.body.height
     let weight = req.body.weight
@@ -23,7 +30,8 @@ app.post("/bmi", (req,res)=>{
     let bmi = weight / height**2
     res.send(`Your BMI is ${bmi.toFixed(2)}.`)
 })
-
+// Route Ends
+// Sever Starts
 app.listen(3000, ()=>{
     console.log("server has started")
 })
