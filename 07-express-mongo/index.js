e0bc2c1e08842b7972670e77453fa82aac201a16
yongsannnn <yong_s@live.com>
2021-02-10 08:19:13 +0000

    added sample recipes

--- 07-express-mongo/index.js
+++ 07-express-mongo/index.js
@@ -5,12 +5,23 @@ const wax = require('wax-on');
 require("dotenv").config();
 const mongoUrl = process.env.MONGO_URL;
 const MongoUtil = require("./MongoUtil");
-
+// create an instance of express app
 let app = express();
+
+app.set('view engine', 'hbs');
+app.use(express.static('public')); 
+wax.on(hbs.handlebars);
+wax.setLayoutPath('./views/layouts')
+app.use(express.urlencoded({
+    extended: false
+}))
+
 //Setup Ends
 
 async function main(){
-    let db = await MongoUtil.connect(mongoUrl, "movie_application")
+    let db = await MongoUtil.connect(mongoUrl, "food_recipes")
+
+    app.get("/")
 }
 main();
 //Sever Starts
